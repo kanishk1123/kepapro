0b6a09ce71cc0313486d19623df341bb453f00b2
kanishk soni <102342556+kanishk1122@users.noreply.github.com>
2024-05-09 20:18:27 +0000

    ascmbakjbs

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -61,7 +61,7 @@ const Home = () => {
         <div className="w-[65%] h-full relative">
           <div className="w-full h-[100vh] flex justify-start">
             <div className="w-[90vw] h-[100em] overflow-hidden" style={{...clipPathStyle, position: 'absolute', backdropFilter: 'blur(30px)'}}>
-              <div className="flex flex-col justify-between h-full  p-20 bg-[rgba(0,0,0,0.8)]">
+              <div className="flex flex-col justify-between h-[100vh]  p-20 bg-[rgba(0,0,0,0.8)]">
                 <div>
                   <p>season 1</p>
                   <h1 className="text-[6vw] font-semibold">name</h1>
