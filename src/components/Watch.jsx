19918f6833ba267a3a6ce8165e4e7fd2aa2cfe14
kanishk soni <102342556+kanishk1122@users.noreply.github.com>
2024-05-09 21:05:21 +0000

    acsnknasc

--- src/components/Watch.jsx
+++ src/components/Watch.jsx
@@ -121,7 +121,7 @@ const desiredPart = parts.slice(4);
         <div className="w-[100vw] h-fit bg-black p-5 flex flex-wrap rounded gap-2">
   {data.map((item, index) => {
     return item.animename === name ? (
-      <Link key={index} to="/">
+      <Link key={index} to={`/watch/${item.animename}/${item.season}/${item.ep}`}>
         <div className="max-w-[440px]  w-[100vw] flex gap-3 rounded p-4 h-[150px] bg-zinc-700">
           <img src={item.thumnail} className="w-1/2 rounded object-cover" alt="" />
           <div className="flex flex-col w-2/3">
