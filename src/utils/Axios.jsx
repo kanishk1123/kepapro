61017fb99678a164ad2268acf4d2a3a1d1a99486
kanishk soni <102342556+kanishk1122@users.noreply.github.com>
2024-05-14 01:37:23 +0530

    Update Axios.jsx

--- src/utils/Axios.jsx
+++ src/utils/Axios.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
     baseURL: "https://kepapro-back.onrender.com",
-})
+    withCredentials: true // Allow cookies to be sent with requests
+});
 
 export default instance;
