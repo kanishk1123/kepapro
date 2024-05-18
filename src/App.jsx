d471ad3b4f0c7666330cd687c8948ca8ad857a5f
kanishk soni <102342556+kanishk1122@users.noreply.github.com>
2024-05-18 13:35:36 +0530

    amin

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import axios from "axios"
 import Routing from './utils/Routing'
-import { SpeedInsights } from "@vercel/speed-insights/next"
+
 
 
 const App = () => {
 
   return (
    <>
-   <SpeedInsights/>
    <Routing/>
    </>
   )
